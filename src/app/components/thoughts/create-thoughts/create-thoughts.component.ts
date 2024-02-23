84a0cf9f4fbe3532c2be0cdb99d12f0814d9f3a6
Fabricio Dias <fabricio270909@gmail.com>
2024-02-23 14:32:33 -0300

    Create form initial structure

---
+++ src/app/components/thoughts/create-thoughts/create-thoughts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-thoughts',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-thoughts.component.html',
+  styleUrl: './create-thoughts.component.css'
+})
+export class CreateThoughtsComponent {
+
+}
