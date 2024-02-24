1a7643c26072889971ba67725f5177d45d29bcd7
Fabricio Dias <fabricio270909@gmail.com>
2024-02-23 22:17:40 -0300

    Thoughts list is in main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
 import {FooterComponent} from "./components/footer/footer.component";
 import {CreateThoughtsComponent} from "./components/thoughts/create-thoughts/create-thoughts.component";
+import {ThoughtsListComponent} from "./components/thoughts/thoughts-list/thoughts-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CreateThoughtsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CreateThoughtsComponent, ThoughtsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
