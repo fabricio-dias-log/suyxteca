b2617a1c3bf79c59b898f71b1fe0106afdb04c38
Fabricio Dias <fabricio270909@gmail.com>
2024-02-23 22:42:58 -0300

    Initial thought card structure

---
+++ src/app/components/thoughts/thought/thought.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thought',
+  standalone: true,
+  imports: [],
+  templateUrl: './thought.component.html',
+  styleUrl: './thought.component.css'
+})
+export class ThoughtComponent {
+
+}
