11ebdced599c7f9cbcc22b50afba6dba645461fe
Fabricio Dias <fabricio270909@gmail.com>
2024-02-23 22:08:51 -0300

    Thoughts list initial commit

---
+++ src/app/components/thoughts/thoughts-list/thoughts-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thoughts-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './thoughts-list.component.html',
+  styleUrl: './thoughts-list.component.css'
+})
+export class ThoughtsListComponent {
+
+}
