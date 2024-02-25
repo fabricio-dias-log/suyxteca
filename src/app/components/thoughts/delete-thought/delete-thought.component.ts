5d321cda8f6d766f19aa1d40cef959f1f3dfa84d
Fabricio Dias <fabricio270909@gmail.com>
2024-02-25 12:15:24 -0300

    Remove thought initial commit

---
+++ src/app/components/thoughts/delete-thought/delete-thought.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-thought',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-thought.component.html',
+  styleUrl: './delete-thought.component.css'
+})
+export class DeleteThoughtComponent {
+
+}
