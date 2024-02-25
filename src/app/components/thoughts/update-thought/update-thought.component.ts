fa7e99328a7e803b352d5ff64bd56ca01d637968
Fabricio Dias <fabricio270909@gmail.com>
2024-02-25 13:11:01 -0300

    Update thought initial commit

---
+++ src/app/components/thoughts/update-thought/update-thought.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-thought',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-thought.component.html',
+  styleUrl: './update-thought.component.css'
+})
+export class UpdateThoughtComponent {
+
+}
